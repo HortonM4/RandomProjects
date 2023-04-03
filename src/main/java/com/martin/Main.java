9e6cad8c883741605028f47603ec59105c2c784e
martinhorton <martinhorton7@gmail.com>
2023-04-03 21:55:05 +0100

    Springboot demo

---
+++ src/main/java/com/martin/Main.java
@@ -0,0 +1,34 @@
+package com.martin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        GreetResponse response = new GreetResponse(
+                "Hello Martin!",
+                List.of("Java", "Python", "Javascript"),
+                new Person("Martin", 33, 5_000)
+                );
+        return response;
+    }
+
+    record Person(String name, int age, double savings){
+
+    }
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){}
+}
